7960486633e5418449031454625be578c0d0b080
YiwenTan99 <yumikotan1999@gmail.com>
2020-12-27 13:10:18 +0800

    about us

---
+++ pages/user/homescreen/about-us.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .aboutus{
+            background-color: #CFD3D6;
+        }
+        ul{
+            list-style-type: none;
+        }
+        li{
+            margin-top: 20px;
+            float: left;
+        }
+        li a{
+            font-family: open sans;
+            font-size: 24px;
+            font-weight: 600;         
+            display: block;
+            color: black;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+        li a:hover:not(.active){
+          background-color: #808B96;
+        }
+        .navRight {
+          float: right;
+        }
+
+    </style>
+</head>
+<body class="aboutus">
+    <ul>
+        <img src="../../../img/Logo.png" alt="Logo.png">
+        <div class="navRight">
+            <li><a href="#home">HEALTH STATUS</a></li>
+            <li><a href="#request">REQUEST</a></li>
+            <li><a href="#status">STATUS</a></li>
+            <li><a href="#profile">PROFILE</a></li>
+            <li><a href="#about">ABOUT US</a></li>
+        </div>
+    </ul>
+    <br>
+    <img src="../../../img/Background.png" alt="Nature" style="width:100%;">
+    <br><br><br>
+
+    <table style="height=100%;">
+        <tr>
+            <th style="width:60%;text-align:justify;font-family:monaco;padding: 15px; opacity: 0.7; font-size: 30px">Req N GO is a one-click system which can be used by everyone staying in Malaysia. It is a system provided for everyone to request to cross the border while we are still under restricted movement control (RMO). It is more convenient to use during the pandemic as it is done online so people do not have to go to the police station to get the form and approval. With req’ n go, everything is done at the tip of your finger anytime and anywhere.
+            <br><br>
+            Our objective is to make sure that everyone can request for crossing the border form wherever you are staying. Looking at the world situation now, we understand that it is safer to stay at home to avoid the threat of covid-19. So instead of going outside, exposing yourself to the threat, we provide an online request form for everyone. If you are enquiring for anything regarding our services, feel free to chat us here or you can contact us via email. We will answer any of your concerns as soon as possible.<th>
+            <th style="width:60%; border: 5px solid #99A3A4;text-align:justify;padding: 20px; font-family:monaco; font-size: 30px;opacity: 0.7 ;border-radius: 25px;"><h1>How We Work</h1>Before making any request from our Req N Go, your need to become a member by filling in your details first and creating an account. All informati given to us are kept as private and confidential. Also, members are required to declare their health status before requesting for crossing the border form. Request to cross the border must be done at least 3 days before the date. It takes around 2-4 days to process your request, either it is approve or otherwise.<br><th>
+        </tr>
+    </table>
+</body>
+</html>
