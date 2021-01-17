4e055ea56ab908cf93cda6e9dae754a173fce5bc
alis1234 <58261135+alis1234@users.noreply.github.com>
2021-01-17 15:16:03 +0800

    Add files via upload

--- pages/user/request/request-main.php
+++ pages/user/request/request-main.php
@@ -2,6 +2,12 @@
 <html>
 <head>
 	<title>Request form page</title>
+  <link rel="stylesheet" type="text/css" href="../../../style/style.css">
+  <script type="text/javascript">
+     function myConfirm(){
+          var answer = window.confirm("Thank you for your submission. We will process your letter soon.");
+     }
+     </script>
 
 	<style>
         ul{
@@ -29,38 +35,6 @@
         .navRight {
             float: right;
         }
-
-        form{
-          margin: auto;
-        }
-        *{
-            box-sizing: border-box;
-          }
-          .rform {
-            padding: 10px;
-            background-color: white;
-            width: 50%;
-            margin: auto;
-            font-size: 20px;
-            border-radius: 5px;
-          }
-        .rform input[type=text] {
-          width: 30%;
-          padding: 12px;
-          border: 1px solid #ccc;
-          border-radius: 4px;
-          resize: vertical;
-          background-color: white;
-        }
-        .ra
-        {
-          text-align: left;
-          background-color: white;
-        }
-        .rform{
-          background-color: white;
-        }
-
   </style>
 
 </head>
@@ -77,43 +51,25 @@
 </ul>
 <p style="background-color: #465865; color: #394d60; margin-top: 0px;"><br><br></p>
 <br>
+
 <div class="rform">
+
 	<form method="post" action="addreq.php" enctype="multipart/form-data">
-		<h1 style="text-align: center;">REQUEST FORM</h1><br>
+    <h1 style="text-align: center; color: black;">REQUEST FORM</h1><br>
+		
 		1. Vehicle Registration No.: <input type="text" name="Vehicle_Req_No" required>
-  		<br><br>
-    
   		
     2. Mode of Transport: <input type="text" name="Mode_Of_Transportation" required>
-  		<br><br>
-      
-  
+  	 
     3. Identity Number: <input type="text" name="Identity_No" required>
-  		<br><br>
      
-  4. Departure Date: <input type="Date" name="Departure_Date" required>
-  		<br><br>
-      
+    4. Departure Date: <input type="Date" name="Departure_Date" required>	      
   
     5. Arrival Date: <input type="Date" name="Arrival_Date" required>
-  		<br><br>
-
-  		  6. Reason: <input type="text" name="Reason" required>
-  		<br><br>
-
-      7. Request Date: <input type="Date" name="Request_Date" required>
-      <br><br>
-
-      8. Form Id: <input type="text" name="Form_Id" required>
-      <br><br>
-
-      9. Staff_Id: <input type="text" name="Staff_Id" required>
-      <br><br>
-
-      <button onclick="alert('Your request will be processed soon!')"><input type="submit" value="submit" name="submit" ></button> 
-
-
-     <!--  <br><br>  <input onclick="alert('Thank you for your submission. Your request will be processed soon.')" type="submit" value="submit" name="submit"><br><br> -->
+  	
+  	6. Reason: <input type="text" name="Reason" required>
+     
+     <button class="button" type="submit" onClick="myConfirm()">Submit</button>
 </form>
 </div>
 <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
