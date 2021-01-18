ac20d8e62b0477edd8881765de5daaff85c388ca
Azri Azmi <azri041@gmail.com>
2021-01-18 12:29:49 +0800

    latest database

--- database/reqngo1.sql
+++ database/reqngo1.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2020 at 08:43 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- Generation Time: Jan 18, 2021 at 05:28 AM
+-- Server version: 10.1.39-MariaDB
+-- PHP Version: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,10 +29,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `Staff_id` int(6) NOT NULL,
+  `Staff_Id` int(6) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
   `Fullname` varchar(30) NOT NULL,
-  `Position` varchar(15) NOT NULL,
-  `Position_Station_No` int(11) NOT NULL
+  `Position` varchar(255) NOT NULL,
+  `Position_Station_No` int(11) NOT NULL,
+  `User_Type` varchar(255) NOT NULL DEFAULT 'Admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -46,19 +50,11 @@ CREATE TABLE `health` (
   `IsCough` varchar(5) NOT NULL,
   `IsSore_Throat` varchar(5) NOT NULL,
   `IsDifficult_Breath` varchar(5) NOT NULL,
-  `OtherSymtoms` varchar(20) NOT NULL,
+  `OtherSymptoms` varchar(20) NOT NULL,
   `CloseContact` varchar(5) NOT NULL,
   `IsRed_Area` varchar(5) NOT NULL,
   `Health_Status` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `health`
---
-
-INSERT INTO `health` (`Form_Id`, `IsFever`, `IsCough`, `IsSore_Throat`, `IsDifficult_Breath`, `OtherSymtoms`, `CloseContact`, `IsRed_Area`, `Health_Status`) VALUES
-(2, 'No', 'No', 'No', 'No', '', 'No', 'No', 'Good'),
-(3, 'No', 'No', 'No', 'No', '', 'No', 'Yes', 'Bad');
 
 -- --------------------------------------------------------
 
@@ -69,11 +65,11 @@ INSERT INTO `health` (`Form_Id`, `IsFever`, `IsCough`, `IsSore_Throat`, `IsDiffi
 CREATE TABLE `request` (
   `Request_Id` int(3) NOT NULL,
   `Identity_No` int(12) NOT NULL,
-  `Vehicle_Req_No` varchar(7) NOT NULL,
+  `Vehicle_Req_No` varchar(255) NOT NULL,
   `Departure_Date` date NOT NULL,
   `Arrival_Date` date NOT NULL,
   `Reason` varchar(100) NOT NULL,
-  `Request_Date` date NOT NULL,
+  `Request_Date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   `Request_Status` varchar(10) NOT NULL,
   `Form_Id` int(5) NOT NULL,
   `Staff_Id` int(6) NOT NULL,
@@ -88,12 +84,15 @@ CREATE TABLE `request` (
 
 CREATE TABLE `user` (
   `Identity_No` int(12) NOT NULL,
+  `Identity_Type` varchar(255) NOT NULL,
   `Fullname` varchar(30) NOT NULL,
   `Address` varchar(30) NOT NULL,
+  `State` varchar(255) NOT NULL,
+  `Nationality` varchar(255) NOT NULL,
   `Email` varchar(15) NOT NULL,
-  `Phone_Number` varchar(11) NOT NULL,
-  `Nationality` varchar(3) NOT NULL,
-  `Identity_Type` varchar(10) NOT NULL
+  `Password` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `Phone_Number` varchar(14) NOT NULL,
+  `User_Type` varchar(256) NOT NULL DEFAULT 'Customer'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -104,7 +103,7 @@ CREATE TABLE `user` (
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`Staff_id`);
+  ADD PRIMARY KEY (`Staff_Id`);
 
 --
 -- Indexes for table `health`
@@ -133,10 +132,22 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `Staff_Id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `health`
 --
 ALTER TABLE `health`
   MODIFY `Form_Id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `request`
+--
+ALTER TABLE `request`
+  MODIFY `Request_Id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -148,7 +159,7 @@ ALTER TABLE `health`
 ALTER TABLE `request`
   ADD CONSTRAINT `fk_foreign_key_name` FOREIGN KEY (`Identity_No`) REFERENCES `user` (`Identity_No`),
   ADD CONSTRAINT `fk_foreign_key_name1` FOREIGN KEY (`Form_Id`) REFERENCES `health` (`Form_Id`),
-  ADD CONSTRAINT `fk_foreign_key_name2` FOREIGN KEY (`Staff_Id`) REFERENCES `admin` (`Staff_id`);
+  ADD CONSTRAINT `fk_foreign_key_name2` FOREIGN KEY (`Staff_Id`) REFERENCES `admin` (`Staff_Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
